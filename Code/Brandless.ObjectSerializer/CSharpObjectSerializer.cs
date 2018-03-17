2e975e8341c177255e09cf729d897f7a4422cd1a
Josh Comley <joshcomley@googlemail.com>
2018-03-17 16:29:12 +0000

    Now uses Brandless dependency

--- Code/Brandless.ObjectSerializer/CSharpObjectSerializer.cs
+++ Code/Brandless.ObjectSerializer/CSharpObjectSerializer.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using Brandless.Extensions;
 using Brandless.ObjectSerializer.Extensions;
 using Brandless.ObjectSerializer.GuidTools;
 using Microsoft.CodeAnalysis;
--- Code/Brandless.ObjectSerializer/CSharpSerializerParameters.cs
+++ Code/Brandless.ObjectSerializer/CSharpSerializerParameters.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using Brandless.Extensions;
+using Brandless.ObjectSerializer.Extensions;
 
 namespace Brandless.ObjectSerializer
 {
